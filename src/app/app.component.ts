e71424992cffbff751fd3f41a2653ba76956bd34
prameshbajra <pe.messh@gmail.com>
2021-09-06 19:02:47 +0545

    Random number generate done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'no-code-mi';
+    title = 'no-code-mi';
+
+    public positions = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"];
+    public triplets = this.getTriplets();
+
+    public genUniqTriplets(): any {
+        const number = Math.ceil(Math.random() * 1000) + "";
+        const setReady = number.split("");
+        if ((new Set(setReady)).size === 3 && !setReady.includes("0")) {
+            return setReady.map(value => parseInt(value));
+        }
+        return this.genUniqTriplets();
+    }
+
+    public getTriplets(): any {
+        const uniqTriplets = this.genUniqTriplets();
+        const positionTriples = this.positions.filter((position, index) => uniqTriplets.includes(index));
+        console.log(positionTriples);
+        return positionTriples;
+    }
+
 }

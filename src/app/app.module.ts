40f1209bcdd044be40a5d86915314551c157e9f3
prameshbajra <pe.messh@gmail.com>
2021-09-06 18:02:14 +0545

    Mi in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      FlexLayoutModule,
+      MatFormFieldModule,
+      MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
